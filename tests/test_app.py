2f466f69d1046a0ff74abb30f2e51e3148ca48f0
Frederick <antvenomxd72@gmail.com>
2023-08-06 01:49:53 -0300

    Implementando rotas CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,65 @@
-from fastapi.testclient import TestClient
-from fast_zero.app import app
+# from fastapi.testclient import TestClient
 
-client = TestClient(app)
+# from fast_zero.app import app
 
 
-def test_root_deve_retornar_200_e_ola_mundo():
-    response = client.get('/')
+def test_root_deve_retornar_200_e_ola_mundo(client):
+    response = client.get('/')  # Act - interage com o sistema
+
+    assert response.status_code == 200  # assert
+    assert response.json() == {'message': 'Olá Mundo!'}   # assert
+
+
+def test_create_user(client):
+    response = client.post(
+        'users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
     assert response.status_code == 200
-    assert response.json() == {'message': 'Olá Mundo!'}
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == 200
+    assert response.json() == {'detail': 'User deleted'}
